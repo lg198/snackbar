e1a52bc7ee87fcb1f50aed2fc8a34df548d69831
Layne Gustafson <lg198@pvsd.org>
2015-05-23 17:45:08 -0400

    Added the main layout.

---
+++ src/com/github/lg198/snackbar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.lg198.snackbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Layne on 5/23/2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
